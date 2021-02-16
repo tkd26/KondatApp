28da0e5d0e6a7c073fdb3ea6382c75a09277d151
takumi-hiraoka <takumi-hiraoka@takumi-hiraokanoMacBook-Air.local>
2021-02-16 22:18:15 +0900

    resoleve error in DateForm.tsx

--- components/molecules/DateForm.tsx
+++ components/molecules/DateForm.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Reacat, {useState, useEffect} from "react";
 
-const DateForm = () => {
-    const [count, counter] = useState(0);
-    let today = new Date();
-    let day = today.getDate();
-    let month = today.getMonth();
+// const DateForm = () => {
+//     const [count, counter] = useState(0);
+//     let today = new Date();
+//     let day = today.getDate();
+//     let month = today.getMonth();
 
-    useEffect( ()=>{
+//     useEffect( ()=>{
 
-    })
-    return (
-        <>
-            <ul>{month}月{day}日</ul>
-        </>
-    )
-};
-export default DateForm;
+//     })
+//     return (
+//         <>
+//             <ul>{month}月{day}日</ul>
+//         </>
+//     )
+// };
+// export default DateForm;
