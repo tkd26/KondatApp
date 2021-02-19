4ba1b118825bb750fb1b0120a2affb2e122ae4b3
takumi-hiraoka <takumi-hiraoka@takumi-hiraokanoMacBook-Air.local>
2021-02-19 13:08:49 +0900

    display raderchart

--- types/nutrition.ts
+++ types/nutrition.ts
@@ -1,8 +1,10 @@
 export type Nutri = {
     id: string;
     genre: string;
-    fat: number;
-    protain: number;
-    water: number;
+    cal: number;
+    fat: number; // 脂質
+    protain: number; // タンパク質
+    carb: number; // 炭水化物
+    na: number; // ナトリウム
+    ca: number; // カリウム
   };
-  
