8fd92dbf59a70a48788f31eed31853c85f510fae
tkd26 <takedam0226@gmail.com>
2021-02-20 20:16:39 +0900

    フォントファミリーの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,10 +10,20 @@ const MyApp = (app: AppProps) => {
   const { Component, pageProps } = app;
 
   return (
+    <>
+    <div className='layout'>
     <Layout>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       <Component {...pageProps} />
     </Layout>
+    </div>
+    <style>{`
+      @import url('https://fonts.googleapis.com/css?family=M+PLUS+1p');
+      .layout {
+        font-family: 'M PLUS 1p', sans-serif;
+      }
+    `}</style>
+    </>
   );
 };
 
