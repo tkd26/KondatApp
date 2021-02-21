a3e3f7214a77c4d610899428d322348ed6ee5001
HirokiTakata <sakuradadadash555@gmail.com>
2021-02-21 18:32:42 +0900

    ロゴをホームページに追加

--- components/page/Main.tsx
+++ components/page/Main.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { Space } from 'antd';
 import {Jumbotron, Button, Card} from 'react-bootstrap'
 
+
 const Main: React.FC = () => {
 
   return (
     <>
       <Space direction="vertical" size="large" style={{ width: '100%' }}>
         <Jumbotron>
-            <h1 style={{fontWeight:700, textAlign:"center"}}>献立アプリへようこそ！</h1>
+            <div className='top-graph'>
+              <img src={'/static/logo-big.png'} style={{ width: '50%', height:'50%'}} />
+            </div>
+            <br></br>
+            <h1 style={{fontWeight:700, textAlign:"center"}}>らくっくへようこそ！</h1>
             <br></br>
             <br></br>
-            <p style={{textAlign:"center"}}>献立アプリは、飲食店と自炊を頑張る人を応援するWebアプリケーションです。</p>
+            <p style={{textAlign:"center"}}>らくっくは、飲食店と自炊を頑張る人を応援するWebアプリケーションです。</p>
             <p style={{textAlign:"center"}}>
             献立を決めてレシピを検索しよう。自炊に疲れた時や特別な日には、食べたい物を入力して、近くのお店を検索しよう。 </p>
             <br></br>
@@ -22,17 +27,9 @@ const Main: React.FC = () => {
       </Space>
 
       <style jsx>{`
-        .form-wrap {
+        .top-graph {
           display: flex;
-          flex-direction: column;
-          margin-bottom: 5%;
-          padding: 3rem;
-          background-color: white;
-        }
-        .btn-wrap {
-          margin-top: 1rem;
-          display: flex;
-          justify-content: flex-end;
+          justify-content: center;
         }
       `}</style>
     </>
